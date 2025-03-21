761e4e86b7da8f5c5582d8449066901689351be1
adellorto <a.dellorto02@gmail.com>
2025-03-21 18:01:42 +0100

    Final update (still not run the simulation)

--- EPA133a-Lab/EPA133a-G12-A3/model/model_run.py
+++ EPA133a-Lab/EPA133a-G12-A3/model/model_run.py
@@ -23,7 +23,7 @@ probabilities = pd.DataFrame({
 probabilities.index = [f"Scenario {i}" for i in range(len(probabilities))]
 
 # Run time: 5 x 24 hours; 1 tick = 1 minute
-run_length = 72000
+run_length = 7200
 
 # Ensure output directory exists
 output_dir = "../experiment"
