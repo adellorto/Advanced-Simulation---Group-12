6928c472b3087a3251a1bc58482f02d3b364cb43
TanguiLG <tangui.lg@protonmail.com>
2025-03-31 15:52:05 +0200

    first bit cleaning done

--- EPA133a-G12-A4/model/data_clean.py
+++ EPA133a-G12-A4/model/data_clean.py
@@ -1,8 +1,25 @@
 import pandas as pd
-path = '../data/RMMS/combined_traffic.csv'
-def clean(filename):
+import os
 
+path = '../data/RMMS/combined_traffic.csv'
+clean_path = '../data/RMMS/combined_traffic_clean.csv'  # Full output path
+
+
+def clean(filename):
     df_traffic = pd.read_csv(filename)
     print(df_traffic.head())
 
-clean(path)
+    # Remove rows where any column contains '*'
+    df_traffic = df_traffic[~df_traffic.apply(lambda row: row.astype(str).str.contains("\*", regex=True).any(), axis=1)]
+
+    # Keep only rows where 'Name' is not NaN
+    df_traffic = df_traffic[df_traffic['Name'].notna()]
+    df_traffic[['Road', 'RL']] = df_traffic['Link no'].str.split('-', expand=True)
+    df_traffic = df_traffic.drop(columns=['Link no'])
+    new_order = ['Road', 'RL'] + [col for col in df_traffic.columns if col not in ['Road', 'RL']]
+    df_traffic = df_traffic[new_order]
+    return df_traffic
+
+
+df_clean = clean(path)
+df_clean.to_csv(clean_path, index=False)  # Save directly to clean_path
