b1d084643be6317757134841d3ffe13f8c9227c7
adellorto <a.dellorto02@gmail.com>
2025-04-01 17:47:43 +0200

    Starting with criticality metrics

--- EPA133a-G12-A4/model/Criticality_scores.py
+++ EPA133a-G12-A4/model/Criticality_scores.py
@@ -9,7 +9,7 @@ df['length'] = np.random.randint(1, 11, size=len(df))
 df["Criticality_scores"] = (df["Traffic"].sum()/df["length"].sum()) - df["Traffic"].sum()/(df["length"].sum()-df["length"])
 df["Criticality_scores"] = (df["Criticality_scores"] / df["Criticality_scores"].min()).round(2)
 
-print(df["Criticality_scores"].unique())
+print(df["Criticality_scores"].mean())
 
 df["Criticality_scores"].plot(kind="hist")
 
