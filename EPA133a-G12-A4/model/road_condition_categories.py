5d8d3085ccd957fa6c8b5538aedd3381a95a9f2c
adaprecup <ada.precup@gmail.com>
2025-04-01 11:57:30 +0200

    Extracted the relevant data to help categorize the road condition

---
+++ EPA133a-G12-A4/model/road_condition_categories.py
@@ -0,0 +1,7 @@
+'''
+    We categorize the roads from A to D based on the number of contracts and the total length of works. A condition of state 'A' represents the highest level of security for the road, while 'D' represents the lowest level of security.
+'''
+
+#Start of by exploring the distribution of the works on the roads
+print(roads_df.describe())
+# Define the conditions
