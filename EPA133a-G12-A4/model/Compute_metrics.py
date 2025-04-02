e7a724b7b08502a91fa86d143fdf39b48bab254d
adellorto <a.dellorto02@gmail.com>
2025-04-02 11:20:41 +0200

    Finished criticality metrics

---
+++ EPA133a-G12-A4/model/Compute_metrics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def compute_criticality(df):
+    df = df.copy()
+    criticality = (df["Traffic"].sum() / df["Length"].sum()) - df["Traffic"].sum() / (df["Length"].sum() - df["Length"])
+    criticality = (criticality / criticality.min())
+    criticality = criticality * df["Length"] / df["Length"].max()
+
+    return criticality
+
+
+
+df = pd.read_csv("../data/processed_data/combined_traffic_clean.csv")
+
+df["Criticality_scores"] = compute_criticality(df)
+
+df["Criticality_scores"].plot(kind="kde")
+plt.show()
