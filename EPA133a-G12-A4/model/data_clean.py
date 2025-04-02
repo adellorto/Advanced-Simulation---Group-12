9370aa0ec1f5d1c8ffd9ad379c5fa25919e9ac8c
TanguiLG <tangui.lg@protonmail.com>
2025-04-02 10:31:48 +0200

    WIP data-clean

--- EPA133a-G12-A4/model/data_clean.py
+++ EPA133a-G12-A4/model/data_clean.py
@@ -5,7 +5,7 @@ path = '../data/RMMS/combined_traffic.csv'
 clean_path = '../data/RMMS/processed_data/combined_traffic_clean.csv'  # Full output path
 
 
-def clean_traffic(filename): #not used for now
+def clean_traffic(filename):
     df_traffic = pd.read_csv(filename)
     print(df_traffic.head())
 
@@ -17,10 +17,11 @@ def clean_traffic(filename): #not used for now
     df_traffic[['Road', 'RL']] = df_traffic['Link no'].str.split('-', expand=True)
     df_traffic = df_traffic.drop(columns=['Link no'])
     new_order = ['Road', 'RL'] + [col for col in df_traffic.columns if col not in ['Road', 'RL']]
-    df_traffic['Start'] = df_traffic[new_order]
     df_traffic = df_traffic[new_order]
+
     return df_traffic
 
+#clean_lrps is not used currently
 def clean_lrps(filename):
     df_lrps = pd.read_csv(filename)
     print(df_lrps.head())
@@ -28,9 +29,12 @@ def clean_lrps(filename):
 
     return df_lrps
 
+#df_assignment3 = pd.read_csv("../../EPA133a-Lab/EPA133a-G12-A3/data/final_input_data.csv")
+#df_assignment3['id'] = df_assignment3['id'].replace("1000", "LRPS")
+
 
 df_clean_traffic = clean_traffic(path)
-df_clean_lrps = clean_lrps(path)
+#df_clean_lrps = clean_lrps(path)
 df_clean_traffic.to_csv(clean_path, index=False)  # Save directly to clean_path
-df_clean_lrps.to_csv(clean_path, index=False)
+#df_clean_lrps.to_csv(clean_path, index=False)
 
