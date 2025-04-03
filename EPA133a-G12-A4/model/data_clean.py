bbb59128115bb2b02a059856da30bf6d069a6d9b
TanguiLG <tangui.lg@protonmail.com>
2025-04-03 14:21:25 +0200

    added a line in data_clean to rename a column for merging, not very useful in the end but makes it more consistent

--- EPA133a-G12-A4/model/data_clean.py
+++ EPA133a-G12-A4/model/data_clean.py
@@ -7,26 +7,29 @@ traffic_clean_path = '../data/processed_data/combined_traffic_clean.csv'  # Full
 
 def clean_traffic(filename):
     df_traffic = pd.read_csv(filename)
-    print(df_traffic.head())
 
     # Remove rows where any column contains '*' (gets rid of columns where road wasn't measured)
     df_traffic = df_traffic[~df_traffic.apply(lambda row: row.astype(str).str.contains("\*", regex=True).any(), axis=1)]
 
     # Keep only rows where 'Name' is not NaN
     df_traffic = df_traffic[df_traffic['Name'].notna()]
+    df_traffic.rename(columns={'Start location': 'LRP No'}, inplace=True)
+
     df_traffic[['Road', 'RL']] = df_traffic['Link no'].str.split('-', expand=True)
     df_traffic = df_traffic.drop(columns=['Link no'])
     new_order = ['Road', 'RL'] + [col for col in df_traffic.columns if col not in ['Road', 'RL']]
     df_traffic = df_traffic[new_order]
 
+
     return df_traffic
 
-#clean_lrps is not used currently
 def clean_lrps(filename):
     df_lrps = pd.read_csv(filename)
     print(df_lrps.head())
     df_lrps = df_lrps.iloc[:, 1:-2]  # Drops first column (index 0) and second-to-last column (-2)
-
+    df_lrps = df_lrps[df_lrps['LRP No'].notna()]
+    df_traffic = pd.read_csv(path)
+    df_lrps['Road'] = df_traffic['Road']
     return df_lrps
 
 #df_assignment3 = pd.read_csv("../../EPA133a-Lab/EPA133a-G12-A3/data/final_input_data.csv")
@@ -40,4 +43,6 @@ df_clean_traffic.to_csv(traffic_clean_path, index=False)  # Save directly to cle
 
 df_clean_lrps = clean_lrps(lrps_path)
 df_clean_lrps.to_csv(lrps_clean_path, index=False)
+
+
 
--- EPA133a-G12-A4/model/data_pull.py
+++ EPA133a-G12-A4/model/data_pull.py
@@ -88,3 +88,4 @@ def parse_lrps():
 
 parse_traffic()
 parse_lrps()
+
