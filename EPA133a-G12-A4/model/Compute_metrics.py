f4bbf6f9f274aac241215d97150f60a8663d24e9
adellorto <a.dellorto02@gmail.com>
2025-04-03 14:49:27 +0200

    Extend criticality metrics to whole roads

--- EPA133a-G12-A4/model/Compute_metrics.py
+++ EPA133a-G12-A4/model/Compute_metrics.py
@@ -7,13 +7,17 @@ def compute_criticality(df):
     criticality = (criticality / criticality.min())
     criticality = criticality * df["Length"] / df["Length"].max()
 
+
     return criticality
 
+def compute_vulnerability(df):
+    ...
+
+    return df
 
 
 df = pd.read_csv("../data/processed_data/combined_traffic_clean.csv")
 
 df["Criticality_scores"] = compute_criticality(df)
 
-df["Criticality_scores"].plot(kind="kde")
-plt.show()
+print(df.groupby("Road")["Criticality_scores"].mean().sort_values(ascending=False).head(10))
